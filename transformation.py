9b6c9e2795b89210ba65c3572fe41d7b79abda14
IvanLudvig <itereshko@list.ru>
2019-12-09 15:16:10 +0300

    work

--- transformation.py
+++ transformation.py
@@ -2,24 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import string
 
-a, b, c, d = (0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 1)
-E = np.array([a, b, d, c])
+V = np.array([(0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)])
 
 A = np.array([[2, 5, 0],
               [-11, 10, 0],
+              [0, 0, 1]])
+A = np.array([[-4, 7, 0],
+              [8, 1, 0],
               [0, 0, 1]])
 c = [(A.item(0, 0) * A.item(0, 1)) + (A.item(1, 0) * A.item(1, 1)),
      -(A.item(1, 1) ** 2) - (A.item(0, 1) ** 2) + (A.item(1, 0) ** 2) + (A.item(0, 0) ** 2),
      -(A.item(0, 0) * A.item(0, 1)) - (A.item(1, 0) * A.item(1, 1))]
 b1, b2 = np.roots(c)
-e1, e2 = [1, b1], [b2, 1]
-ne1, ne2 = np.matmul((A[0:2, 0:2]), e1), np.matmul((A[0:2, 0:2]), e2)
-angle = np.arccos(np.dot(ne1, e1) / (np.linalg.norm(ne1) * np.linalg.norm(e1)))
-scalex = np.linalg.norm(ne1) / np.linalg.norm(e1)
-scaley = np.linalg.norm(ne2) / np.linalg.norm(e2)
+e1, e2 = np.array([1, b1, 1]), np.array([b1, -1, 1])
+print(e1, e2)
+ne1, ne2 = np.matmul(A, e1), np.matmul(A, e2)
+angle = np.arccos(np.dot(ne1[0:2], e1[0:2]) / (np.linalg.norm(ne1[0:2]) * np.linalg.norm(e1[0:2])))
+scalex = np.linalg.norm(ne1[0:2]) / np.linalg.norm(e1[0:2])
+scaley = np.linalg.norm(ne2[0:2]) / np.linalg.norm(e2[0:2])
+
 print("rotate by: ", angle)
-print("scale by ", scalex, " to ", ne1.item(0), "x", '{:+f}'.format(ne1.item(1)).rstrip("0"), "y", "=0")
-print("scale by ", scaley, " to ", ne2.item(0), "x", '{:+f}'.format(ne2.item(1)).rstrip("0"), "y", "=0")
+print("scale by ", scalex, " to ", '{:+f}'.format(ne1.item(0)).rstrip("0").rstrip("."), "x",
+      '{:+f}'.format(ne1.item(1)).rstrip("0").rstrip("."), "y", "= 0")
+print("scale by ", scaley, " to ", '{:+f}'.format(ne2.item(0)).rstrip("0").rstrip("."), "x",
+      '{:+f}'.format(ne2.item(1)).rstrip("0").rstrip("."), "y", "= 0")
+
+sc = np.diag([scalex, scaley, 1])
+rot = np.array([[np.cos(angle), -np.sin(angle), 0],
+                [np.sin(angle), np.cos(angle), 0],
+                [0, 0, 1]])
+p = np.array([[ne1.item(0), ne2.item(0), 0], [ne1.item(1), ne2.item(1), 0], [0, 0, 1]])
+print(p)
+sc = np.matmul(p, sc)
+sc = np.matmul(sc, np.linalg.inv(p))
+# sc = np.transpose(sc)
+# print(sc)
+u = (np.matmul(sc, rot))
+print(u)
 X = []
 Y = []
 X1 = []
@@ -27,15 +46,15 @@ Y1 = []
 fig = plt.figure()
 ax = plt.gca()
 
-for v in E:
-    transform = np.eye(3) @ v
+for v in V:
+    transform = A @ v
     x, y, z = transform
     X.append(x)
     Y.append(y)
     plt.scatter(X, Y)
 
-for v in E:
-    transform = A @ v
+for v in V:
+    transform = u @ v
     x, y, z = transform
     X1.append(x)
     Y1.append(y)
